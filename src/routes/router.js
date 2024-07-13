cbfa3204145358497f1c80d04320168b9252954a
Sabino Capingana <scapingana@gmail.com>
2024-07-13 11:34:48 +0100

    certo

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,12 +5,21 @@ const router = express.Router();
 router.get("/", (request, response) => {
   response.status(200).render("home", { titulo: "Pagina de inicio" });
 });
+
 router.get("/candidatos-inscritos", (request, response) => {
   response
     .status(200)
     .render("candidatos", {
       titulo: "Página de candidatos",
       lista_de_candidatos: "Lista de candidatos inscritos",
+    });
+});
+router.get("/cadastrar-candidato", (request, response) => {
+  response
+    .status(200)
+    .render("formCandidato", {
+      titulo: "Formulário para inscrição",
+      lista_de_candidatos: "Inscrever Candidato",
     });
 });
 
